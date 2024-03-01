5e1475176da578e3eebefe3caeecf9de34343ca6
emil-98 <ewelton98@gmail.com>
2024-03-01 13:39:22 -0700

    update look of mobile menu, add public dev command

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import {FaBars, FaGithub, FaLinkedin, FaXmark} from "react-icons/fa6"
 import { HiMail } from 'react-icons/hi'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from "../components/Logo"
 import ToggleSwitch from './ToggleSwitch'
 import { NavLink } from 'react-router-dom'
@@ -42,12 +41,12 @@ const Navbar = () => {
       </div>
 
       {/* Mobile Menu */}
-      <ul className={`overflow-hidden shadow-[0_0_1000px_rgba(0,0,0,1)] transition-all text-2xl text-gray-400 absolute z-40 top-0 end-0 h-screen bg-slate-950 bg-opacity-70 backdrop-blur-lg flex flex-col space-y-6 items-start ${!nav ? 'w-0 p-0 text-transparent' : 'w-5/6 p-6'}`}>
-        <li onClick={handleClick} className='py-6'><NavLink className={({isActive}) => isActive ? underline: noUnderline} to='/'>Home</NavLink></li>
-        <li onClick={handleClick} className='py-6'><NavLink className={({isActive}) => isActive ? underline: noUnderline} to='/about'>About</NavLink></li>
-        <li onClick={handleClick} className='py-6'><NavLink className={({isActive}) => isActive ? underline: noUnderline} to='/skills'>Skills</NavLink></li>
-        <li onClick={handleClick} className='py-6'><NavLink className={({isActive}) => isActive ? underline: noUnderline} to='/projects'>Projects</NavLink></li>
-        <li onClick={handleClick} className='py-6'><NavLink className={({isActive}) => isActive ? underline: noUnderline} to='/contact'>Contact</NavLink></li>
+      <ul className={`overflow-hidden shadow-[0_0_1000px_rgba(0,0,0,1)] transition-all text-2xl absolute z-40 top-0 end-0 h-screen bg-white dark:bg-slate-950 bg-opacity-70 backdrop-blur-lg flex flex-col space-y-6 items-start ${!nav ? 'w-0 p-0 text-transparent' : 'w-5/6 p-6'}`}>
+        <li onClick={handleClick} className='py-6'><NavLink className={({isActive}) => isActive ? underline : noUnderline} to='/'>Home</NavLink></li>
+        <li onClick={handleClick} className='py-6'><NavLink className={({isActive}) => isActive ? underline : noUnderline} to='/about'>About</NavLink></li>
+        <li onClick={handleClick} className='py-6'><NavLink className={({isActive}) => isActive ? underline : noUnderline} to='/skills'>Skills</NavLink></li>
+        <li onClick={handleClick} className='py-6'><NavLink className={({isActive}) => isActive ? underline : noUnderline} to='/projects'>Projects</NavLink></li>
+        <li onClick={handleClick} className='py-6'><NavLink className={({isActive}) => isActive ? underline : noUnderline} to='/contact'>Contact</NavLink></li>
       </ul>
 
 
@@ -55,17 +54,17 @@ const Navbar = () => {
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-100 bg-blue-500">
-            <a href="https://www.linkedin.com/in/emil-welton" className="flex justify-between items-center w-full text-white dark:text-gray-200">
+            <a href="https://www.linkedin.com/in/emil-welton" className="flex justify-between items-center w-full text-white">
               LinkedIn <FaLinkedin size={30}/>
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-100 bg-gray-900">
-            <a href="https://github.com/emil-98" className="flex justify-between items-center w-full text-white dark:text-gray-200">
+            <a href="https://github.com/emil-98" className="flex justify-between items-center w-full text-white">
               Github <FaGithub size={30}/>
             </a>
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-100 bg-red-600">
-            <a href="mailto:ewelton98@gmail.com" className="flex justify-between items-center w-full text-white dark:text-gray-200">
+            <a href="mailto:ewelton98@gmail.com" className="flex justify-between items-center w-full text-white">
               Email <HiMail size={30}/>
             </a>
           </li>
